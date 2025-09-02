f0c235c931967a010c1ae1654987f66f88b368c6
Serpentine2128 <benedictroshan41319@gmail.com>
2025-09-02 11:40:55 +0530

    Test1 sep2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['../demo-styling.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'angular-quickstart';
